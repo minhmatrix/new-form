c5a2f1231da0990aba6f57eef89897d9d6f73754
minhmatrix <125466698+minhmatrix@users.noreply.github.com>
2026-03-11 09:20:14 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 const TOKEN = '8010774541:AAH_AtsUiUycqtpTGi8lMSAtsbQK4FpgtrY';
-const CHAT_ID = '-5270877913';
+const CHAT_ID = '-1003726345500';
 
 const POST = async (req: NextRequest) => {
     try {
